77a5ce8f13e72f9ea07504dd3866ef43996694f2
keyur <keyurpansuriya88@outlook.com>
2024-01-07 15:24:33 +0530

    New Url

--- src/app/lib/studentdb.js
+++ src/app/lib/studentdb.js
@@ -1,6 +1,8 @@
+import { env } from "../../../next.config";
+
 const username ="keyurpansuriya88";
 const password ="0DaT3QFaXRiby8VZ";
 
 export const Connect_url = "mongodb+srv://"+username+":"+password+"@cluster0.4brsisv.mongodb.net/taskstudentDB?retryWrites=true&w=majority";
 
-export const BASE_API_URL = "http://localhost:3000";
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASIC_API_URL;
